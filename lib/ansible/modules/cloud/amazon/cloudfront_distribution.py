501a8c4d78af5ab5369350b96b996b487dc00a17
Will Thames <will@thames.id.au>
2018-07-03 08:54:48 +1000

    Handle FieldLevelEncryptionId in cloudfront_distribution
    Ensure that FieldLevelEncryptionId is properly handled - passing it if
    set, and keeping it if returned by GetDistribution
    
    Update cloudfront_distribution tests to remove references to
    test_identifier so test suite actually works
    
    Fixes #40724

--- lib/ansible/modules/cloud/amazon/cloudfront_distribution.py
+++ lib/ansible/modules/cloud/amazon/cloudfront_distribution.py
@@ -17,7 +17,8 @@ description:
     - Allows for easy creation, updating and deletion of CloudFront distributions.
 
 requirements:
-  - boto3 >= 1.0.0
+  - boto3 >= 1.6.0
+  - botocore >= 1.9.22
   - python >= 2.6
 
 version_added: "2.5"
@@ -152,6 +153,7 @@ options:
               I(lambda_function_associations[])
                 I(lambda_function_arn)
                 I(event_type)
+              I(field_level_encryption_id)
 
     cache_behaviors:
       description:
@@ -180,6 +182,7 @@ options:
             I(max_ttl)
             I(compress)
             I(lambda_function_associations[])
+            I(field_level_encryption_id)
 
     purge_cache_behaviors:
       description: Whether to remove any cache behaviors that aren't listed in I(cache_behaviors). This switch
@@ -1505,6 +1508,7 @@ class CloudFrontValidationManager(object):
         cache_behavior = self.validate_allowed_methods(config, cache_behavior.get('allowed_methods'), cache_behavior)
         cache_behavior = self.validate_lambda_function_associations(config, cache_behavior.get('lambda_function_associations'), cache_behavior)
         cache_behavior = self.validate_trusted_signers(config, cache_behavior.get('trusted_signers'), cache_behavior)
+        cache_behavior = self.validate_field_level_encryption_id(config, cache_behavior.get('field_level_encryption_id'), cache_behavior)
         return cache_behavior
 
     def validate_cache_behavior_first_level_keys(self, config, cache_behavior, valid_origins, is_default_cache):
@@ -1586,6 +1590,14 @@ class CloudFrontValidationManager(object):
             return cache_behavior
         except Exception as e:
             self.module.fail_json_aws(e, msg="Error validating lambda function associations")
+
+    def validate_field_level_encryption_id(self, config, field_level_encryption_id, cache_behavior):
+        # only set field_level_encryption_id if it's already set or if it was passed
+        if field_level_encryption_id is not None:
+            cache_behavior['field_level_encryption_id'] = field_level_encryption_id
+        elif 'field_level_encryption_id' in config:
+            cache_behavior['field_level_encryption_id'] = config.get('field_level_encryption_id')
+        return cache_behavior
 
     def validate_allowed_methods(self, config, allowed_methods, cache_behavior):
         try:
