f466811e63034ecc02b36929a82e437de9d37bfe
The Magician <magic-modules@google.com>
2018-08-20 11:25:12 -0700

    Partial updates (#63)

--- lib/ansible/modules/cloud/google/gcp_compute_forwarding_rule.py
+++ lib/ansible/modules/cloud/google/gcp_compute_forwarding_rule.py
@@ -263,168 +263,145 @@ EXAMPLES = '''
 '''
 
 RETURN = '''
-creationTimestamp:
-  description:
-  - Creation timestamp in RFC3339 text format.
-  returned: success
-  type: str
-description:
-  description:
-  - An optional description of this resource. Provide this property when you create
-    the resource.
-  returned: success
-  type: str
-id:
-  description:
-  - The unique identifier for the resource.
-  returned: success
-  type: int
-IPAddress:
-  description:
-  - The IP address that this forwarding rule is serving on behalf of.
-  - Addresses are restricted based on the forwarding rule's load balancing scheme
-    (EXTERNAL or INTERNAL) and scope (global or regional).
-  - When the load balancing scheme is EXTERNAL, for global forwarding rules, the address
-    must be a global IP, and for regional forwarding rules, the address must live
-    in the same region as the forwarding rule. If this field is empty, an ephemeral
-    IPv4 address from the same scope (global or regional) will be assigned. A regional
-    forwarding rule supports IPv4 only. A global forwarding rule supports either IPv4
-    or IPv6.
-  - When the load balancing scheme is INTERNAL, this can only be an RFC 1918 IP address
-    belonging to the network/subnet configured for the forwarding rule. By default,
-    if this field is empty, an ephemeral internal IP address will be automatically
-    allocated from the IP range of the subnet or network configured for this forwarding
-    rule.
-  - 'An address can be specified either by a literal IP address or a URL reference
-    to an existing Address resource. The following examples are all valid: * 100.1.2.3
-    * U(https://www.googleapis.com/compute/v1/projects/project/regions/region/addresses/address)
-    * projects/project/regions/region/addresses/address * regions/region/addresses/address
-    * global/addresses/address * address .'
-  returned: success
-  type: str
-IPProtocol:
-  description:
-  - The IP protocol to which this rule applies. Valid options are TCP, UDP, ESP, AH,
-    SCTP or ICMP.
-  - When the load balancing scheme is INTERNAL, only TCP and UDP are valid.
-  returned: success
-  type: str
-backendService:
-  description:
-  - A BackendService to receive the matched traffic. This is used only for INTERNAL
-    load balancing.
-  returned: success
-  type: dict
-ipVersion:
-  description:
-  - ipVersion is not a valid field for regional forwarding rules.
-  returned: success
-  type: str
-loadBalancingScheme:
-  description:
-  - 'This signifies what the ForwardingRule will be used for and can only take the
-    following values: INTERNAL, EXTERNAL The value of INTERNAL means that this will
-    be used for Internal Network Load Balancing (TCP, UDP). The value of EXTERNAL
-    means that this will be used for External Load Balancing (HTTP(S) LB, External
-    TCP/UDP LB, SSL Proxy) .'
-  returned: success
-  type: str
-name:
-  description:
-  - Name of the resource; provided by the client when the resource is created. The
-    name must be 1-63 characters long, and comply with RFC1035. Specifically, the
-    name must be 1-63 characters long and match the regular expression `[a-z]([-a-z0-9]*[a-z0-9])?`
-    which means the first character must be a lowercase letter, and all following
-    characters must be a dash, lowercase letter, or digit, except the last character,
-    which cannot be a dash.
-  returned: success
-  type: str
-network:
-  description:
-  - For internal load balancing, this field identifies the network that the load balanced
-    IP should belong to for this Forwarding Rule. If this field is not specified,
-    the default network will be used.
-  - This field is only used for INTERNAL load balancing.
-  returned: success
-  type: dict
-portRange:
-  description:
-  - This field is used along with the target field for TargetHttpProxy, TargetHttpsProxy,
-    TargetSslProxy, TargetTcpProxy, TargetVpnGateway, TargetPool, TargetInstance.
-  - Applicable only when IPProtocol is TCP, UDP, or SCTP, only packets addressed to
-    ports in the specified range will be forwarded to target.
-  - Forwarding rules with the same [IPAddress, IPProtocol] pair must have disjoint
-    port ranges.
-  - 'Some types of forwarding target have constraints on the acceptable ports: * TargetHttpProxy:
-    80, 8080 * TargetHttpsProxy: 443 * TargetTcpProxy: 25, 43, 110, 143, 195, 443,
-    465, 587, 700, 993, 995, 1883, 5222 * TargetSslProxy: 25, 43, 110, 143, 195, 443,
-    465, 587, 700, 993, 995, 1883, 5222 * TargetVpnGateway: 500, 4500 .'
-  returned: success
-  type: str
-ports:
-  description:
-  - This field is used along with the backend_service field for internal load balancing.
-  - When the load balancing scheme is INTERNAL, a single port or a comma separated
-    list of ports can be configured. Only packets addressed to these ports will be
-    forwarded to the backends configured with this forwarding rule.
-  - You may specify a maximum of up to 5 ports.
-  returned: success
-  type: list
-subnetwork:
-  description:
-  - The subnetwork that the load balanced IP should belong to for this Forwarding
-    Rule. This field is only used for INTERNAL load balancing.
-  - If the network specified is in auto subnet mode, this field is optional. However,
-    if the network is in custom subnet mode, a subnetwork must be specified.
-  returned: success
-  type: dict
-target:
-  description:
-  - This field is only used for EXTERNAL load balancing.
-  - A reference to a TargetPool resource to receive the matched traffic.
-  - This target must live in the same region as the forwarding rule.
-  - The forwarded traffic must be of a type appropriate to the target object.
-  returned: success
-  type: dict
-allPorts:
-  description:
-  - For internal TCP/UDP load balancing (i.e. load balancing scheme is INTERNAL and
-    protocol is TCP/UDP), set this to true to allow packets addressed to any ports
-    to be forwarded to the backends configured with this forwarding rule. Used with
-    backend service. Cannot be set if port or portRange are set.
-  returned: success
-  type: bool
-networkTier:
-  description:
-  - 'The networking tier used for configuring this address. This field can take the
-    following values: PREMIUM or STANDARD. If this field is not specified, it is assumed
-    to be PREMIUM.'
-  returned: success
-  type: str
-serviceLabel:
-  description:
-  - An optional prefix to the service name for this Forwarding Rule.
-  - If specified, will be the first label of the fully qualified service name.
-  - The label must be 1-63 characters long, and comply with RFC1035.
-  - Specifically, the label must be 1-63 characters long and match the regular expression
-    `[a-z]([-a-z0-9]*[a-z0-9])?` which means the first character must be a lowercase
-    letter, and all following characters must be a dash, lowercase letter, or digit,
-    except the last character, which cannot be a dash.
-  - This field is only used for INTERNAL load balancing.
-  returned: success
-  type: str
-serviceName:
-  description:
-  - The internal fully qualified service name for this Forwarding Rule.
-  - This field is only used for INTERNAL load balancing.
-  returned: success
-  type: str
-region:
-  description:
-  - A reference to the region where the regional forwarding rule resides.
-  - This field is not applicable to global forwarding rules.
-  returned: success
-  type: str
+    creation_timestamp:
+        description:
+            - Creation timestamp in RFC3339 text format.
+        returned: success
+        type: str
+    description:
+        description:
+            - An optional description of this resource. Provide this property when you create
+              the resource.
+        returned: success
+        type: str
+    id:
+        description:
+            - The unique identifier for the resource.
+        returned: success
+        type: int
+    ip_address:
+        description:
+            - The IP address that this forwarding rule is serving on behalf of.
+            - Addresses are restricted based on the forwarding rule's load balancing scheme (EXTERNAL
+              or INTERNAL) and scope (global or regional).
+            - When the load balancing scheme is EXTERNAL, for global forwarding rules, the address
+              must be a global IP, and for regional forwarding rules, the address must live in
+              the same region as the forwarding rule. If this field is empty, an ephemeral IPv4
+              address from the same scope (global or regional) will be assigned. A regional forwarding
+              rule supports IPv4 only. A global forwarding rule supports either IPv4 or IPv6.
+            - When the load balancing scheme is INTERNAL, this can only be an RFC 1918 IP address
+              belonging to the network/subnet configured for the forwarding rule. By default,
+              if this field is empty, an ephemeral internal IP address will be automatically allocated
+              from the IP range of the subnet or network configured for this forwarding rule.
+            - 'An address can be specified either by a literal IP address or a URL reference to
+              an existing Address resource. The following examples are all valid:  * 100.1.2.3
+              * U(https://www.googleapis.com/compute/v1/projects/project/regions/region/addresses/address)
+              * projects/project/regions/region/addresses/address * regions/region/addresses/address
+              * global/addresses/address * address .'
+        returned: success
+        type: str
+    ip_protocol:
+        description:
+            - The IP protocol to which this rule applies. Valid options are TCP, UDP, ESP, AH,
+              SCTP or ICMP.
+            - When the load balancing scheme is INTERNAL, only TCP and UDP are valid.
+        returned: success
+        type: str
+    backend_service:
+        description:
+            - A reference to a BackendService to receive the matched traffic.
+            - This is used for internal load balancing.
+            - "(not used for external load balancing) ."
+        returned: success
+        type: dict
+    ip_version:
+        description:
+            - The IP Version that will be used by this forwarding rule. Valid options are IPV4
+              or IPV6. This can only be specified for a global forwarding rule.
+        returned: success
+        type: str
+    load_balancing_scheme:
+        description:
+            - 'This signifies what the ForwardingRule will be used for and can only take the following
+              values: INTERNAL, EXTERNAL The value of INTERNAL means that this will be used for
+              Internal Network Load Balancing (TCP, UDP). The value of EXTERNAL means that this
+              will be used for External Load Balancing (HTTP(S) LB, External TCP/UDP LB, SSL Proxy)
+              .'
+        returned: success
+        type: str
+    name:
+        description:
+            - Name of the resource; provided by the client when the resource is created. The name
+              must be 1-63 characters long, and comply with RFC1035. Specifically, the name must
+              be 1-63 characters long and match the regular expression `[a-z]([-a-z0-9]*[a-z0-9])?`
+              which means the first character must be a lowercase letter, and all following characters
+              must be a dash, lowercase letter, or digit, except the last character, which cannot
+              be a dash.
+        returned: success
+        type: str
+    network:
+        description:
+            - For internal load balancing, this field identifies the network that the load balanced
+              IP should belong to for this Forwarding Rule. If this field is not specified, the
+              default network will be used.
+            - This field is not used for external load balancing.
+        returned: success
+        type: dict
+    port_range:
+        description:
+            - This field is used along with the target field for TargetHttpProxy, TargetHttpsProxy,
+              TargetSslProxy, TargetTcpProxy, TargetVpnGateway, TargetPool, TargetInstance.
+            - Applicable only when IPProtocol is TCP, UDP, or SCTP, only packets addressed to
+              ports in the specified range will be forwarded to target.
+            - Forwarding rules with the same [IPAddress, IPProtocol] pair must have disjoint port
+              ranges.
+            - 'Some types of forwarding target have constraints on the acceptable ports:  * TargetHttpProxy:
+              80, 8080 * TargetHttpsProxy: 443 * TargetTcpProxy: 25, 43, 110, 143, 195, 443, 465,
+              587, 700, 993, 995,                   1883, 5222 * TargetSslProxy: 25, 43, 110,
+              143, 195, 443, 465, 587, 700, 993, 995,                   1883, 5222 * TargetVpnGateway:
+              500, 4500 .'
+        returned: success
+        type: str
+    ports:
+        description:
+            - This field is used along with the backend_service field for internal load balancing.
+            - When the load balancing scheme is INTERNAL, a single port or a comma separated list
+              of ports can be configured. Only packets addressed to these ports will be forwarded
+              to the backends configured with this forwarding rule.
+            - You may specify a maximum of up to 5 ports.
+        returned: success
+        type: list
+    subnetwork:
+        description:
+            - A reference to a subnetwork.
+            - For internal load balancing, this field identifies the subnetwork that the load
+              balanced IP should belong to for this Forwarding Rule.
+            - If the network specified is in auto subnet mode, this field is optional. However,
+              if the network is in custom subnet mode, a subnetwork must be specified.
+            - This field is not used for external load balancing.
+        returned: success
+        type: dict
+    target:
+        description:
+            - A reference to a TargetPool resource to receive the matched traffic.
+            - For regional forwarding rules, this target must live in the same region as the forwarding
+              rule. For global forwarding rules, this target must be a global load balancing resource.
+              The forwarded traffic must be of a type appropriate to the target object.
+            - This field is not used for internal load balancing.
+        returned: success
+        type: dict
+    label_fingerprint:
+        description:
+            - The fingerprint used for optimistic locking of this resource.  Used internally during
+              updates.
+        returned: success
+        type: str
+    region:
+        description:
+            - A reference to the region where the regional forwarding rule resides.
+            - This field is not applicable to global forwarding rules.
+        returned: success
+        type: str
 '''
 
 ################################################################################
@@ -477,7 +454,7 @@ def main():
     if fetch:
         if state == 'present':
             if is_different(module, fetch):
-                fetch = update(module, self_link(module), kind)
+                fetch = update(module, self_link(module), kind, fetch)
                 changed = True
         else:
             delete(module, self_link(module), kind)
@@ -500,8 +477,41 @@ def create(module, link, kind):
     return wait_for_operation(module, auth.post(link, resource_to_request(module)))
 
 
-def update(module, link, kind):
-    module.fail_json(msg="ForwardingRule cannot be edited")
+def update(module, link, kind, fetch):
+    update_fields(module, resource_to_request(module),
+                  response_to_hash(module, fetch))
+    return fetch_resource(module, self_link(module), kind)
+
+
+def update_fields(module, request, response):
+    if response.get('target') != request.get('target'):
+        target_update(module, request, response)
+
+
+def target_update(module, request, response):
+    auth = GcpSession(module, 'compute')
+    auth.post(
+        ''.join([
+            "https://www.googleapis.com/compute/v1/",
+            "projects/{project}/regions/{region}/forwardingRules/{name}/setTarget"
+        ]).format(**module.params),
+        {
+            u'target': replace_resource_dict(module.params.get(u'target', {}), 'selfLink')
+        }
+    )
+
+
+def label_fingerprint_update(module, request, response):
+    auth = GcpSession(module, 'compute')
+    auth.post(
+        ''.join([
+            "https://www.googleapis.com/compute/v1/",
+            "projects/{project}/regions/{region}/forwardingRules/{name}/setLabels"
+        ]).format(**module.params),
+        {
+            u'labelFingerprint': response.get('labelFingerprint')
+        }
+    )
 
 
 def delete(module, link, kind):
@@ -606,10 +616,7 @@ def response_to_hash(module, response):
         u'ports': response.get(u'ports'),
         u'subnetwork': response.get(u'subnetwork'),
         u'target': response.get(u'target'),
-        u'allPorts': response.get(u'allPorts'),
-        u'networkTier': module.params.get('network_tier'),
-        u'serviceLabel': response.get(u'serviceLabel'),
-        u'serviceName': response.get(u'serviceName'),
+        u'labelFingerprint': response.get(u'labelFingerprint')
     }
 
 
